51ea59fc6d3d6ae61fc9503c69455b812493d3c9
Bunmi <bunmioye09@gmail.com>
2022-03-01 20:33:14 +0100

    create header styled components

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import {
+  HeaderContainer,
+  HeaderWrap,
+  HeaderLogo,
+  HeaderLogoImg,
+  HeaderMenu,
+  HeaderItem,
+  HeaderLink,
+} from './HeaderElements';
+import logo from '../../assets/logo.svg';
 
 function Header() {
   return (
-    <header>
-      Header Placeholder
-    </header>
+    <HeaderContainer>
+      <HeaderWrap>
+        <HeaderLogo to="/">
+          <HeaderLogoImg src={logo} alt="" />
+        </HeaderLogo>
+        <HeaderMenu>
+          <HeaderItem>
+            <HeaderLink to="/search">Search</HeaderLink>
+          </HeaderItem>
+          <HeaderItem>
+            <HeaderLink to="/how-it-works">How it works</HeaderLink>
+          </HeaderItem>
+          <HeaderItem>
+            <HeaderLink to="/about">About</HeaderLink>
+          </HeaderItem>
+        </HeaderMenu>
+      </HeaderWrap>
+    </HeaderContainer>
   );
 }
 
