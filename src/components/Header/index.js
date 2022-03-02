643e12e2809131a961bf306acb6c03adf7ba6450
Bunmi <bunmioye09@gmail.com>
2022-03-02 22:16:26 +0100

    add header element (trial)

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -12,24 +12,26 @@ import logo from '../../assets/logo.svg';
 
 function Header() {
   return (
-    <HeaderContainer>
-      <HeaderWrap>
-        <HeaderLogo to="/">
-          <HeaderLogoImg src={logo} alt="" />
-        </HeaderLogo>
-        <HeaderMenu>
-          <HeaderItem>
-            <HeaderLink to="/search/javascript">Search</HeaderLink>
-          </HeaderItem>
-          <HeaderItem>
-            <HeaderLink to="/#how-it-works">How it works</HeaderLink>
-          </HeaderItem>
-          <HeaderItem>
-            <HeaderLink to="/#about">About</HeaderLink>
-          </HeaderItem>
-        </HeaderMenu>
-      </HeaderWrap>
-    </HeaderContainer>
+    <header>
+      <HeaderContainer>
+        <HeaderWrap>
+          <HeaderLogo to="/">
+            <HeaderLogoImg src={logo} alt="" />
+          </HeaderLogo>
+          <HeaderMenu>
+            <HeaderItem>
+              <HeaderLink to="/search/javascript">Search</HeaderLink>
+            </HeaderItem>
+            <HeaderItem>
+              <HeaderLink to="/#how-it-works">How it works</HeaderLink>
+            </HeaderItem>
+            <HeaderItem>
+              <HeaderLink to="/#about">About</HeaderLink>
+            </HeaderItem>
+          </HeaderMenu>
+        </HeaderWrap>
+      </HeaderContainer>
+    </header>
   );
 }
 
