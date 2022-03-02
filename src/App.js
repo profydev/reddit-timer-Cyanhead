95be30d25733a67b79b6f42daadf0357aed36111
Bunmi <bunmioye09@gmail.com>
2022-03-02 12:42:06 +0100

    add hash routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
+import GlobalStyle from './GlobalStyles';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Header />}>
-          <Route path="/search" element="" />
-        </Route>
-        <Route path="*" element="" />
-      </Routes>
-    </Router>
+    <>
+      <GlobalStyle />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Header />}>
+            <Route path="/javascript" element="" />
+            <Route path="/how-it-works" element="" />
+            <Route path="/about" element="" />
+          </Route>
+          <Route path="*" element="" />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
