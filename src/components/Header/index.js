cbf835b25e8fbbee29815c9b0fe43009d0049be9
Bunmi <bunmioye09@gmail.com>
2022-03-04 23:45:51 +0100

    fix failed tests (attempt 04)

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,19 +1,19 @@
 import React from 'react';
-// import {
-//   HeaderContainer,
-//   HeaderWrap,
-//   HeaderLogo,
-//   HeaderLogoImg,
-//   HeaderMenu,
-//   HeaderItem,
-//   HeaderLink,
-// } from './HeaderElements';
+import {
+  HeaderContainer,
+  HeaderWrap,
+  HeaderLogo,
+  HeaderLogoImg,
+  HeaderMenu,
+  HeaderItem,
+  HeaderLink,
+} from './HeaderElements';
 import logo from '../../assets/logo.svg';
 
 function Header() {
   return (
     <>
-      <header>
+      {/* <header>
         <nav>
           <a href="/">
             <img src={logo} alt="" />
@@ -30,26 +30,26 @@ function Header() {
             </li>
           </ul>
         </nav>
-      </header>
+      </header> */}
 
-      {/* <HeaderContainer>
-      <HeaderWrap>
-        <HeaderLogo to="/">
-          <HeaderLogoImg src={logo} alt="" />
-        </HeaderLogo>
-        <HeaderMenu>
-          <HeaderItem>
-            <HeaderLink to="search/javascript">Search</HeaderLink>
-          </HeaderItem>
-          <HeaderItem>
-            <HeaderLink to="#how-it-works">How it works</HeaderLink>
-          </HeaderItem>
-          <HeaderItem>
-            <HeaderLink to="#about">About</HeaderLink>
-          </HeaderItem>
-        </HeaderMenu>
-      </HeaderWrap>
-    </HeaderContainer> */}
+      <HeaderContainer>
+        <HeaderWrap>
+          <HeaderLogo to="/">
+            <HeaderLogoImg src={logo} alt="" />
+          </HeaderLogo>
+          <HeaderMenu>
+            <HeaderItem>
+              <HeaderLink to="search/javascript">Search</HeaderLink>
+            </HeaderItem>
+            <HeaderItem>
+              <HeaderLink to="#how-it-works">How it works</HeaderLink>
+            </HeaderItem>
+            <HeaderItem>
+              <HeaderLink to="#about">About</HeaderLink>
+            </HeaderItem>
+          </HeaderMenu>
+        </HeaderWrap>
+      </HeaderContainer>
     </>
   );
 }
